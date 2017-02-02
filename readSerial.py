82b4daa5d3d2c3d31e00718350e7515e96be13af
armageddom <jere.kylatie@hotmail.com>
2017-02-02 09:51:55 +0000

    Muokattu etta lisaa kaiken tietokantaan oikein

--- readSerial.py
+++ readSerial.py
@@ -11,14 +11,14 @@ connected = True
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 
-#dbc = ("localhost", "root", "pi", "projekti")
-#db = MySQLdb.connect(dbc[0],dbc[1],dbc[2],dbc[3])
-#curs = db.cursor()
+dbc = ("localhost", "root", "raspberry", "projekti")
+db = MySQLdb.connect(dbc[0],dbc[1],dbc[2],dbc[3])
+curs = db.cursor()
 
-#idQuery = "SELECT id FROM Mittaukset ORDER BY id DESC LIMIT 1"
-#curs.execute(idQuery)
+idQuery = "SELECT id FROM Mittaukset ORDER BY id DESC LIMIT 1"
+curs.execute(idQuery)
 
-#viiminen = int(curs.fetchone()[0])
+viiminen = int(curs.fetchone()[0])
 
 
 
@@ -32,15 +32,15 @@ address = 0x68
 #-------------------------------------------------------------------------------------------------------
 
 
-#tiedot = ("INSERT INTO Mittaukset" 
-#            "(id, aika, lampo, kosteus)"
-#            "VALUES (%s,%s,%s,%s)")
-#laskuri = 0 
-#laskuri = viiminen
+tiedot = ("INSERT INTO Mittaukset" 
+            "(id, aika, kosteus,latitudeDegree,latitude,longitudedegree,longitude)"
+            "VALUES (%s,%s,%s,%s,%s,%s,%s)")
+laskuri = 0 
+laskuri = viiminen
 
-#while not connected:
-#        serin = ser.read()
-#        connected = True
+while not connected:
+        serin = ser.read()
+        connected = True
 
 if (connected == True):
         print "Connected!"
@@ -77,7 +77,7 @@ def get_double(register):
 
 
 while 1:
-#        laskuri = laskuri + 1 
+        laskuri = laskuri + 1 
     
     
     
@@ -86,7 +86,7 @@ while 1:
 
         tm = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
- # Read time from module and display
+        # Read time from module and display
         hours = get_double(0)
         minutes = get_double(2)
         seconds = get_double(4)
@@ -94,12 +94,12 @@ while 1:
 
         time.sleep(1)
 
-    # Read Heading from module and display
+        # Read Heading from module and display
         heading = float(get_single(44))*100
         heading += float(get_single(45))*10
         heading += float(get_single(46))
         heading += float(get_single(47))/10
-        print 'heading: %.1f' %(heading)
+       # print 'heading: %.1f' %(heading)
 
         time.sleep(1)
         # Read speed from module and display
@@ -107,7 +107,7 @@ while 1:
         speed += float(get_single(53))*10
         speed += float(get_single(54))
         speed += float(get_single(55))/10
-        print 'speed: %.1f' %(speed)
+        #print 'speed: %.1f' %(speed)
 
         time.sleep(1)
 
@@ -119,7 +119,7 @@ while 1:
         latitude_minutes += float(get_single(20))/1000
         latitude_minutes += float(get_single(21))/10000
         latitude_direction = chr(get_single(22))
-        print 'latitude: %.0f %.5f %s' %(latitude_degrees,latitude_minutes,latitude_direction)
+    #    print 'latitude: %.0f %.5f %s' %(latitude_degrees,latitude_minutes,latitude_direction)
 
         time.sleep(1)
 
@@ -132,38 +132,47 @@ while 1:
         longitude_minutes += float(get_single(30))/1000
         longitude_minutes += float(get_single(31))/10000
         longitude_direction = chr(get_single(32))
-        print 'longitude: %.0f %.5f %s' %(longitude_degrees,longitude_minutes,longitude_direction)
+     #   print 'longitude: %.0f %.5f %s' %(longitude_degrees,longitude_minutes,longitude_direction)
 
         time.sleep(1)                                                                                                                  
 
 
+        latitudeDeg = latitude_degrees
+        latitude = latitude_minutes
+        longitudeDeg = longitude_degrees
+        longitude = longitude_minutes
+
+        #print latitude
+        #print longitude
 
 
+        kosteus = ser.readline()
+        #kosteus = arvot
 
-
-
-#        arvot = ser.readline()
-#        kosteus, lampo = arvot.split(",")
-
- #       kosteus = float(kosteus)
-#        lampo = float(lampo)
         
-    #    print "Huoneen lampo: %.2f"  % lampo
-    #   print "Huoneen kosteus: %.2f" % kosteus
+        latitudeDeg = int(latitudeDeg)
+        latitude = float(latitude)
+
+        longitudeDeg = int(latitudeDeg)
+        longitude = float(latitude)        
+
+        kosteus = float(kosteus)
+    #   lampo = float(lampo)
+        
+    #   print "Huoneen lampo: %.2f"  % lampo
+        print "Huoneen kosteus: %.2f" % kosteus
         
            
 
-        #data_tiedot = (laskuri,tm,lampo,kosteus)
+        data_tiedot = (laskuri,tm,kosteus,latitudeDeg,latitude,longitudeDeg,longitude)
 
-#        try:
-#            curs.execute (tiedot,data_tiedot)
-#            db.commit()
-#        except:
-#            print "meni vituiksi"
-#           db.rollback()
-        #if laskuri >= 100:
-        #    curs.execute("TRUNCATE TABLE Mittaukset")
-        #    laskuri = 1
-
-
+        try:
+            curs.execute(tiedot,data_tiedot)
+            db.commit()
+        except:
+            print "meni vituiksi"
+            db.rollback()
+        if laskuri >= 100:
+            curs.execute("TRUNCATE TABLE Mittaukset")
+            laskuri = 1
                                                                                                                                                      
