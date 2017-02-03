086d7b85890c5f1e339f3fb927e2de12eeac1f55
Aku <aku.jaakola@luukku.com>
2017-02-03 11:23:24 +0200

    Djangoa jatkettu ja toimiva arduino-koodi

--- Djangosite/graph/urls.py
+++ Djangosite/graph/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^kosteus/$', views.kosteus, name='kosteus'),
+	url(r'^kartta/$', views.kartta, name='kartta'),
 ]
